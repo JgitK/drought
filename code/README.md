16519ffb7e27c7e3df806d44ff672be7aeaf86cc
Jackson Kent <jk@jacksonkent.co>
2023-11-07 09:02:11 -0800

    Adding files for tar extraction; updated README.md; environment with new dependencies for DAG

---
+++ code/README.md
@@ -0,0 +1,8 @@
+11/4/23
+Created bash scripts for getting data from NOAA via wget 
+
+11/5/23
+Created snakemake file to automate file download with dependencies acct'd for
+
+11/7/23
+Used "archive" package to extract tar, files; ghcnd_all is too big to decompress, would take 17 days.
---
+++ code/read_dly_files.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(archive)
+library(glue)
+
+quadruple <- function(x){
+
+    c(glue("VALUE{x}"), glue("MFLAG{x}"), glue("QFLAG{x}"), glue("SFLAG{x}"))
+    
+}
+
+dly_files <- archive("data/ghcnd_all.tar.gz") |>
+    filter(str_detect(path, ".dly")) |>
+    slice_sample(n = 5) |>
+    pull(path) 
+head(dly_files)
+#list.files("data/ghcnd_all", full.names = TRUE)
+
+widths <- c(11, 4, 2, 4, rep(c(5,1,1,1), 31))
+headers <- c("ID", "YEAR", "MONTH", "ELEMENT",  unlist(map(1:31, quadruple)))
+
+dly_files |>
+    map_dfr(., read_fwf(archive_read("data/ghcnd_all.tar.gz", .x),
+            fwf_widths(widths, headers),
+            na = c("NA", "-9999"),
+            col_types =cols(.default=col_character()),
+            col_select = c("ID", "YEAR", "MONTH", "ELEMENT", starts_with("VALUE")))) 
+            |>
+        rename_all(tolower) |>
+        filter(element == "PRCP") |>
+        select(-element) |>
+        pivot_longer(cols = starts_with("value"),
+            names_to = "day",
+            values_to = "prcp") |>
+            drop_na() |>
+            mutate(day = str_replace(day, "value", ""),
+                    date = ymd(glue("{year}-{month}-{day}")),
+                    prcp_cm = as.numeric(prcp)/100) |>
+                    select(id, date, prcp_cm)
+
+
+
+
+
