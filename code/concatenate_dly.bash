f2a84c5b5d616f30f5a19e2e25573a8c1387f2a0
Jackson Kent <jk@jacksonkent.co>
2023-11-15 16:36:56 -0800

    merged precipitation data wtih station data

--- code/concatenate_dly.bash
+++ code/concatenate_dly.bash
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-### tar Oxvzf practice.tar.gz > practice.output # practice output is intermediate file we do not want
-### gzip -f practice.output
 mkdir -p data/temp
 
-### extract tar files, run through grep that takes any line that contains "PRCP" (precipitation)then compress and store in directory
-tar Oxvzf data/ghcnd_all.tar.gz | grep "PRCP" | split -l 1000000 -d -a 2 - "data/temp/PRCP_chunk_" 
+tar Oxvzf data/ghcnd_all.tar.gz | grep "PRCP" | split -l 500000 -d -a 2 - "data/temp/PRCP_chunk_" 
 gzip data/temp/PRCP_chunk_*
 
 code/read_split_dly_files.R
 
 rm -rf data/temp
+
+### extract tar files, run through grep that takes any line that contains "PRCP" (precipitation)then compress and store in directory
+
+### tar Oxvzf practice.tar.gz > practice.output # practice output is intermediate file we do not want
+### gzip -f practice.output
--- code/read_split_dly_files.R
+++ code/read_split_dly_files.R
@@ -40,23 +40,25 @@ process_xfiles <- function(x) {
             pivot_longer(cols = starts_with("value"),
                 names_to = "day",
                 values_to = "prcp") |>
-                drop_na() |>
-                filter(prcp != 0) |>
+                #drop_na() |>
+                #filter(prcp != 0) |>
                 mutate(day = str_replace(day, "value", ""),
-                        date = ymd(glue("{year}-{month}-{day}")),
-                        prcp_cm = as.numeric(prcp)/100) |>
-                        select(id, date, prcp_cm) |>
-                        mutate(julian_day = yday(date),
-                                    diff = tday_julian - julian_day,
-                                    is_in_window = case_when((diff < window) & (diff > 0) ~ TRUE,
-                                                            diff > window ~ FALSE,
-                                                            tday_julian < window & diff + 365 < window ~ TRUE,
-                                                            diff < 0 ~ FALSE),
-                                    year = year(date),
-                                    year = if_else(diff < 0, year + 1, year)) |>
-                        filter(is_in_window) |>
-                        group_by(id, year) |>
-                        summarize(prcp_cm = sum(prcp_cm), .groups = "drop") #|>
+                        date = ymd(glue("{year}-{month}-{day}"), quiet=TRUE),
+                        prcp = replace_na(prcp, "0"),
+                        prcp = as.numeric(prcp)/100) |> #prcp now in cm
+                drop_na(date) |>
+                select(id, date, prcp) |>
+                mutate(julian_day = yday(date),
+                        diff = tday_julian - julian_day,
+                        is_in_window = case_when((diff < window) & (diff > 0) ~ TRUE,
+                                                  diff > window ~ FALSE,
+                                                  tday_julian < window & diff + 365 < window ~ TRUE,
+                                                  diff < 0 ~ FALSE),
+                        year = year(date),
+                        year = if_else(diff < 0, year + 1, year)) |>
+                filter(is_in_window) |>
+                group_by(id, year) |>
+                summarize(prcp = sum(prcp), .groups = "drop") #|>
                     # write_tsv("data/composite_dly.tsv")
 
 }
@@ -65,5 +67,5 @@ x_files <- list.files("data/temp", full.names=T)
 
 map_dfr(x_files, process_xfiles) |>
 group_by(id, year) |>
-summarize(prcp = sum(prcp_cm), .groups = "drop") |>
+summarize(prcp = sum(prcp ), .groups = "drop") |>
 write_tsv("data/ghcnd_tidy.tsv.gz")
